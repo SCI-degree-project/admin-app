c22611718396fb0327964a5e677b6bf4c63bf3be
Leonardo Herrera <leonardoherrera2304@gmail.com>
2025-07-07 01:03:43 -0400

    feat: styles in login and validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Login from "./modules/auth/components/Login";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Catalog } from "./modules/catalog/components/Catalog";
 import { NotFound } from "./modules/core/components/NotFound";
@@ -9,20 +8,42 @@ import 'react-toastify/dist/ReactToastify.css';
 import CreateProduct from "./modules/catalog/components/CreateProduct";
 import EditProduct from "./modules/catalog/components/EditProduct";
 import { Analytics } from "./modules/analytics/components/Analytics";
+import LoginForm from "./modules/auth/components/LoginForm";
+import ProtectedRoute from "./modules/auth/components/ProtectedRoute";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to="/catalog" />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginForm />} />
 
         <Route element={<Layout />}>
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/new-product" element={<CreateProduct />} />
-          <Route path="/product/:productId" element={<ProductDetails />} />
-          <Route path="/product/edit/:productId" element={<EditProduct />} />
-          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/catalog" element={
+            <ProtectedRoute>
+              <Catalog />
+            </ProtectedRoute>
+          } />
+          <Route path="/new-product" element={
+            <ProtectedRoute>
+              <CreateProduct />
+            </ProtectedRoute>
+          } />
+          <Route path="/product/:productId" element={
+            <ProtectedRoute>
+              <ProductDetails />
+            </ProtectedRoute>
+          } />
+          <Route path="/product/edit/:productId" element={
+            <ProtectedRoute>
+              <EditProduct />
+            </ProtectedRoute>
+          } />
+          <Route path="/analytics" element={
+            <ProtectedRoute>
+              <Analytics />
+            </ProtectedRoute>
+          } />
         </Route>
 
         <Route path="*" element={<NotFound />} />
